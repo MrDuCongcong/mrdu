0546985de3e051fbd454a432bf021ecd247d43dd
DuCongcong <ducongcong1996@163.com>
2023-08-16 07:49:04 +0800

    添加面试目录

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -4,11 +4,12 @@ import { defaultTheme } from 'vuepress';
 export default defineUserConfig({
 	lang: 'zh-CN',
 	title: "MrDu's Blog",
+	lastUpdated: '更新时间',
 	theme: defaultTheme({
 		navbar: [
 			{
 				text: '博客',
-				link: '/blog/',
+				link: '/blog/微前端.md',
 			},
 			{
 				text: '笔记',
@@ -16,7 +17,7 @@ export default defineUserConfig({
 			},
 			{
 				text: '面试',
-				link: '/interview/'
+				link: '/interview/react.md'
 			}
 		],
 		sidebar: {
@@ -25,8 +26,50 @@ export default defineUserConfig({
 			   '前端监控',
 			   '手写常用的工具函数',
 			],
-			'/interview': [
-				'react'
+			'/note/': [
+				{
+                    text: '基础知识',
+                    link: '/base/',
+					children: [
+						{
+							text: 'JavaScript',
+                            link: '/javascript/',
+							children: [
+								'类型',
+								'函数',
+								'对象',
+							]    
+						},
+                        {
+                            text: 'CSS',
+                            link: '/css/',
+							children: [
+                                '基础知识',
+                                'flex布局',
+                                '栅格布局',
+							]
+                        },
+						
+					]
+                },
+				{
+                    text: '框架',
+                    link: '/framework/',
+					children: [
+						{
+							text: 'React',
+                            link: '/react/',
+						},
+						{
+                            text: 'Vue',
+                            link: '/vue/',
+                        },
+					]
+                },
+			],
+			'/interview/': [
+				'TypeScript',
+				'react',
 			]
 		},
 	}),
